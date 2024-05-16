093c318ea6a4a2e4c96e117344d49f878131afb5
Caio Lucas <lienscarlet@gmail.com>
2024-05-16 18:17:31 -0300

    Add fetch answer comment controller

---
+++ src/infra/http/controllers/fetch-answer-comments.controller.ts
@@ -0,0 +1,41 @@
+import { FetchAnswerCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-answer-comments';
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation.pipe';
+import {
+	BadRequestException,
+	Controller,
+	Get,
+	Param,
+	Query,
+} from '@nestjs/common';
+import { z } from 'zod';
+import { commentPresenterToHttp } from '../presenters/comment-presenter';
+
+const pageQueryParamSchema = z.coerce.number().min(1).default(1);
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema);
+
+@Controller('/answers/:answerId/comments')
+export class FetchAnswerCommentsController {
+	constructor(
+		private readonly fetchAnswerComments: FetchAnswerCommentsUseCase,
+	) {}
+
+	@Get()
+	async handle(
+		@Query('page', queryValidationPipe) page: number,
+		@Param('answerId') answerId: string,
+	) {
+		const result = await this.fetchAnswerComments.execute({
+			page,
+			answerId,
+		});
+
+		if (result.isLeft()) {
+			throw new BadRequestException();
+		}
+
+		const comments = result.value.answerComments.map(commentPresenterToHttp);
+
+		return { comments };
+	}
+}
---
+++ tests/controllers/fetch-answer-comments.controller.e2e-test.ts
@@ -0,0 +1,80 @@
+import { AppModule } from '@/infra/app.module';
+import { DatabaseModule } from '@/infra/database/database.module';
+import { INestApplication } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+import { AnswerFactory } from 'tests/domain/forum/application/factories/make-answer';
+import { AnswerCommentFactory } from 'tests/domain/forum/application/factories/make-answer-comment';
+import { QuestionFactory } from 'tests/domain/forum/application/factories/make-question';
+import { StudentFactory } from 'tests/domain/forum/application/factories/make-student';
+
+describe('Fetch Answers AnswerComments (E2E)', () => {
+	let app: INestApplication;
+	let studentFactory: StudentFactory;
+	let questionFactory: QuestionFactory;
+	let answerFactory: AnswerFactory;
+	let answerCommentFactory: AnswerCommentFactory;
+	let jwt: JwtService;
+
+	beforeAll(async () => {
+		const moduleRef = await Test.createTestingModule({
+			imports: [AppModule, DatabaseModule],
+			providers: [
+				StudentFactory,
+				AnswerFactory,
+				AnswerCommentFactory,
+				QuestionFactory,
+			],
+		}).compile();
+
+		app = moduleRef.createNestApplication();
+
+		studentFactory = moduleRef.get(StudentFactory);
+		questionFactory = moduleRef.get(QuestionFactory);
+		answerFactory = moduleRef.get(AnswerFactory);
+		answerCommentFactory = moduleRef.get(AnswerCommentFactory);
+		jwt = moduleRef.get(JwtService);
+
+		app.init();
+	});
+
+	test('[GET] /answers/:answerId/answerComments', async () => {
+		const user = await studentFactory.makePrismaStudent();
+
+		const accessToken = jwt.sign({ sub: user.id.toValue() });
+
+		const question = await questionFactory.makePrismaQuestion({
+			authorId: user.id,
+		});
+
+		const answer = await answerFactory.makePrismaAnswer({
+			authorId: user.id,
+			questionId: question.id,
+		});
+
+		const answerComments = await Promise.all([
+			answerCommentFactory.makePrismaAnswerComment({
+				authorId: user.id,
+				answerId: answer.id,
+			}),
+			answerCommentFactory.makePrismaAnswerComment({
+				authorId: user.id,
+				answerId: answer.id,
+			}),
+		]);
+
+		const response = await request(app.getHttpServer())
+			.get(`/answers/${answer.id.toValue()}/comments`)
+			.set('Authorization', `Bearer ${accessToken}`)
+			.send();
+
+		expect(response.statusCode).toBe(200);
+		expect(response.body).toEqual({
+			comments: expect.arrayContaining([
+				expect.objectContaining({ content: answerComments[0].content }),
+				expect.objectContaining({ content: answerComments[1].content }),
+			]),
+		});
+	});
+});
